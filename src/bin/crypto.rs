4e7c063a8322c46703989ce696c3a0de7243785b
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2025-11-30 17:44:36 +0800

    Put Logger into Handler

--- src/bin/crypto.rs
+++ src/bin/crypto.rs
@@ -205,66 +205,78 @@ impl SharedObject for Crypto {
     }
 }
 
-fn setup_logger() {
-    let level_filter = match (Path::new("trace").exists(), Path::new("debug").exists()) {
-        (true, true) | (true, false) => LevelFilter::Trace,
-        (false, true) => LevelFilter::Debug,
-        (false, false) => LevelFilter::Info, // Default level
-    };
+struct LogHandler;
 
-    if let Err(e) = Dispatch::new()
-        .format(move |out, message, record| {
-            let file = record.file().unwrap_or("unknown_file");
-            let line = record.line().map_or(0, |l| l);
+impl LogHandler {
+    fn start() -> Self {
+        let level_filter = match (Path::new("trace").exists(), Path::new("debug").exists()) {
+            (true, true) | (true, false) => LevelFilter::Trace,
+            (false, true) => LevelFilter::Debug,
+            (false, false) => LevelFilter::Info, // Default level
+        };
 
-            match level_filter {
-                LevelFilter::Off
-                | LevelFilter::Error
-                | LevelFilter::Warn
-                | LevelFilter::Debug
-                | LevelFilter::Trace => {
-                    out.finish(format_args!(
-                        "[{}][{}]: {} <{}:{}>",
-                        Local::now().format("%b-%d-%Y %H:%M:%S.%f"),
-                        record.level(),
-                        message,
-                        file,
-                        line,
-                    ));
+        if let Err(e) = Dispatch::new()
+            .format(move |out, message, record| {
+                let file = record.file().unwrap_or("unknown_file");
+                let line = record.line().map_or(0, |l| l);
+
+                match level_filter {
+                    LevelFilter::Off
+                    | LevelFilter::Error
+                    | LevelFilter::Warn
+                    | LevelFilter::Debug
+                    | LevelFilter::Trace => {
+                        out.finish(format_args!(
+                            "[{}][{}]: {} <{}:{}>",
+                            Local::now().format("%b-%d-%Y %H:%M:%S.%f"),
+                            record.level(),
+                            message,
+                            file,
+                            line,
+                        ));
+                    }
+                    LevelFilter::Info => {
+                        out.finish(format_args!(
+                            "[{}]: {} <{}:{}>",
+                            record.level(),
+                            message,
+                            file,
+                            line,
+                        ));
+                    }
                 }
-                LevelFilter::Info => {
-                    out.finish(format_args!(
-                        "[{}]: {} <{}:{}>",
-                        record.level(),
-                        message,
-                        file,
-                        line,
-                    ));
-                }
-            }
-        })
-        .level(level_filter)
-        .chain(std::io::stdout())
-        .apply()
-    {
-        log::error!("Logger initialization failed: {e:?}");
+            })
+            .level(level_filter)
+            .chain(std::io::stdout())
+            .apply()
+        {
+            log::error!("Logger initialization failed: {e:?}");
+        }
+        let name = env!("CARGO_PKG_NAME");
+        let version = env!("CARGO_PKG_VERSION");
+        log::info!("{name} {version} has started...");
+        Self
     }
 }
-
+impl Drop for LogHandler {
+    fn drop(&mut self) {
+        let name = env!("CARGO_PKG_NAME");
+        let version = env!("CARGO_PKG_VERSION");
+        log::info!("{name} {version} has ended...");
+        log::logger().flush();
+    }
+}
 // replace broken tail with a proper async main
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    setup_logger();
+    let logger = LogHandler::start();
 
     WorkerBuilder::new()
         .add("applications.crypto", Crypto)
         .spawn()
         .await?;
 
-    let name = env!("CARGO_PKG_NAME");
-    let version = env!("CARGO_PKG_VERSION");
-    eprintln!("{name} {version} has ended...");
-
+    drop(logger);
     Ok(())
 }
 
