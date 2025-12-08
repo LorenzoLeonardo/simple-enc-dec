0abc00f9f1a9e8b3dab789b03fd2d486df49c7cd
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2025-12-08 17:22:09 +0800

    Add --progress for file decryption

--- src/bin/decrypt-file.rs
+++ src/bin/decrypt-file.rs
@@ -1,18 +1,36 @@
-use enzo_crypto::file_gcm::decrypt_file;
-use std::{error::Error, path::PathBuf}; // assuming your crate name is enzo_crypto
+use enzo_crypto::{async_file_gcm, file_gcm::decrypt_file};
+use std::{
+    error::Error,
+    io::{Write, stdout},
+    path::PathBuf,
+    time::Duration,
+}; // assuming your crate name is enzo_crypto
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = std::env::args().collect();
 
-    if args.len() != 4 {
-        eprintln!("Usage: {} <input path> <output path> <password>", args[0]);
+    if args.len() < 4 {
+        eprintln!(
+            "Usage: {} <input path> <output path> <password> [--progress]",
+            args[0]
+        );
         std::process::exit(1);
     }
 
     let input_path = PathBuf::from(&args[1]);
     let output_path = PathBuf::from(&args[2]);
-    let password = &args[3];
-
+    let password = args[3].to_string();
+    let progress = if args.len() > 4 {
+        if &args[4] == "--progress" {
+            true
+        } else {
+            eprintln!("Unknown option: {}", args[4]);
+            std::process::exit(1);
+        }
+    } else {
+        false
+    };
     // Check if input path exists and is a file
     if !input_path.exists() {
         eprintln!(
@@ -30,8 +48,37 @@ fn main() -> Result<(), Box<dyn Error>> {
         std::process::exit(1);
     }
 
-    decrypt_file(input_path.as_path(), output_path.as_path(), password)?;
-
+    if progress {
+        let (tx_progress, mut rx_progress) =
+            tokio::sync::mpsc::channel::<enzo_crypto::async_file_gcm::Progress>(10);
+        let dec_task = tokio::task::spawn({
+            let plain = input_path.clone();
+            let enc = output_path.clone();
+            async move {
+                async_file_gcm::decrypt_file(
+                    &plain,
+                    &enc,
+                    &password,
+                    tx_progress,
+                    Duration::from_millis(500),
+                )
+                .await
+            }
+        });
+        while let Some(p) = rx_progress.recv().await {
+            print!(
+                "\rDecrypting: {} bytes of {} bytes ({:.2}%)",
+                p.bytes_processed(),
+                p.total_bytes(),
+                p.percentage()
+            );
+            stdout().flush()?;
+        }
+        dec_task.await??;
+        println!(); // move to new line at the end
+    } else {
+        decrypt_file(input_path.as_path(), output_path.as_path(), &password)?;
+    }
     println!(
         "Decryption successful. Decrypted file saved to: {}",
         output_path.display()
